b1c86cd7b66a9b2dbc9888742f84bb48d549ec3e
IowanEASFan's Random Coding Shenanigans <46287209+MarshmelloSUCKS@users.noreply.github.com>
2025-07-13 16:04:05 -0500

    Update script's built-in readme

--- LimeSqueezer.sh
+++ LimeSqueezer.sh
@@ -7,17 +7,21 @@
 #                           |_|
 #                                    by msx.gay
 #
-#   LimeSqueezer - a script to turn your perfect lossless
+#    LimeSqueezer - a script to turn your perfect lossless
 # CD collection into a hodgepodge pirated mess. Uses FFmpeg
 # to compress your files based on randomly chosen profiles.
 # Uses 64kbps-128kbps MP3 and 64kbps-128kbps WMA.
 #
-# This script only needs FFmpeg, basic GNU tools, and a bash
-# shell to work.
-# If needed, install ffmpeg using your distro's package
-# manager.
+#   This script needs FFmpeg, basic GNU tools, and a bash
+# shell to work. If needed, install ffmpeg using your
+# distro's package manager.
 #
-# queer-coded in 2025 by msx.gay
+#   To use, download LimeSqueezer.sh, place it in a folder
+# with the music you intend to convert, mark the script as
+# executable by running "chmod +x LimeSqueezer.sh", and
+# execute using "./LimeSqueezer.sh".
+#
+# queer-coded in 2025 by msx.gay - http://msx.gay
 
 #Print some kick-ass ASCII art
 echo " __    _           _____"
